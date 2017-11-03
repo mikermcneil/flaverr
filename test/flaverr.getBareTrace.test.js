dbd818b882c519d6983cec28054f464174c20588
Mike McNeil <michael.r.mcneil@gmail.com>
2017-11-02 22:42:33 -0500

    Stub out some more tests.

--- test/flaverr.getBareTrace.test.js
+++ test/flaverr.getBareTrace.test.js
@@ -5,4 +5,13 @@ describe('flaverr.getBareTrace()', ()=>{
     it('should return the provided error\'s trace');
   });
 
+  describe('with `flaverr.getBareTrace()`', ()=>{
+    it('should return a good trace from where it was invoked');
+  });
+
+  describe('with `flaverr.getBareTrace(caller)`', ()=>{
+    it('should fail with an error message explaining about Error.captureStackTrace if the caller is an unrelated function not even from the current stack');
+    it('should return a good trace from where the caller function was invoked in the current stack');
+  });
+
 });
